fe6253b922cc05e921815a003289c96784b2e7d6
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-08 14:49:07 +0300

    configured all endpoints, added a controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -23,4 +27,14 @@ public class DemoApplication extends SpringBootServletInitializer {
 	public TimedAspect timedAspect(MeterRegistry registry){
 		return new TimedAspect(registry);
 	}
+
+	@Timed(value = "greeting.time", description = "The time taken to return the greeting method")
+	public String sayHello(){
+		return "Saying hello from this side";}
+
+	@GetMapping("/working")
+	public String sayhello(){
+		return "Checking if prometheus is working";
+	}
 }
+
