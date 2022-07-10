4ec8d7785df9a4e34b95e27af920fd8bc7f0a242
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-10 23:45:47 +0300

    return created status

--- src/main/java/com/example/demo/Controller/UserResource.java
+++ src/main/java/com/example/demo/Controller/UserResource.java
@@ -3,8 +3,11 @@ package com.example.demo.Controller;
 import com.example.demo.Service.UserDAOService;
 import com.example.demo.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,7 +25,9 @@ public class UserResource {
         return userDAOService.findOne(id);
     }
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser = userDAOService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
