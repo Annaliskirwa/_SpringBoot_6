d30a00d799c5bb1186ed81cc73d7a72ae91658e7
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-10 15:58:16 +0300

    added controller

--- src/main/java/com/example/demo/Controller/UserResource.java
+++ src/main/java/com/example/demo/Controller/UserResource.java
@@ -1,8 +1,9 @@
-package Controller;
+package com.example.demo.Controller;
 
 import com.example.demo.Service.UserDAOService;
 import com.example.demo.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class UserResource {
     @Autowired
     private UserDAOService userDAOService;
 
+    @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userDAOService.findAll();
     }
