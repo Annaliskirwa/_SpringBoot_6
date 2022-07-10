68898b5088493c3c4d6dfd5b2c6966e99cddeb6b
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-10 15:41:08 +0300

    methods to find all and add users

--- src/main/java/com/example/demo/Service/UserDAOService.java
+++ src/main/java/com/example/demo/Service/UserDAOService.java
@@ -10,11 +10,25 @@ import java.util.List;
 @Component
 public class UserDAOService {
     private static List<User> users = new ArrayList<>();
+    private static int userCount = 3;
     static {
         users.add(new User(1, "Annalis", new Date()));
         users.add(new User(2, "Anna", new Date()));
         users.add(new User(3, "Ann", new Date()));
     }
+    //find all users
+    public List<User> findAll(){
+        return users;
+    }
+    //save user
+    public User save(User user){
+        if(user.getId() == null){
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+    //find one user
 
 
 }
