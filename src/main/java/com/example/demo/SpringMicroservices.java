ab6cabe417980470e5445782e0a9b734871dfeb1
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-10 15:27:17 +0300

    returning path variables in a string

--- src/main/java/com/example/demo/SpringMicroservices.java
+++ src/main/java/com/example/demo/SpringMicroservices.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -35,6 +36,10 @@ public class SpringMicroservices extends SpringBootServletInitializer {
 	@GetMapping(path = "hello-world-bean")
 	public HelloWorldBean helloWorldBean(){
 		return new HelloWorldBean("Returning hello world from a bean");
+	}
+	@GetMapping(path = "hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+		return new HelloWorldBean(String.format("Returning hello world from a bean, %s", name));
 	}
 }
 
