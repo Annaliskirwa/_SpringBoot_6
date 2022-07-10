754cf13968a66290a13789f32cf71a66d0bd8756
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-11 01:28:48 +0300

    added validation constraints

--- src/main/java/com/example/demo/Entities/User.java
+++ src/main/java/com/example/demo/Entities/User.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 
 //user data access object(DAO)
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
@@ -15,6 +17,8 @@ import java.util.Date;
 @AllArgsConstructor
 public class User {
     private Integer id;
+    @Size(min=2)
     private String name;
+    @Past
     private Date birthDate;
 }
