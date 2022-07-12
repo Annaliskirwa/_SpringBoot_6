5deab5e4d48cade50b3bb0dc1de6071e7aeae2ad
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-13 00:01:03 +0300

    versioning services using uris

---
+++ src/main/java/com/example/demo/Versioning/PersonVersioniningController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioniningController {
+    @GetMapping("v1/person")
+    public PersonV1 personV1(){
+        return new PersonV1("Annalis Kirwa");
+    }
+    @GetMapping("v2/person")
+    public PersonV2 personV2(){
+        return new PersonV2(new Name("Annalis","Kirwa"));
+    }
+}
