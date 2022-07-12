16b26301883bfa722cfc71e42339df42367f3827
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-12 10:46:06 +0300

    returning a list of fields, hide field json ignore

--- src/main/java/com/example/demo/Controller/FilteringController.java
+++ src/main/java/com/example/demo/Controller/FilteringController.java
@@ -4,10 +4,17 @@ import com.example.demo.Entities.SomeBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class FilteringController {
     @GetMapping("/filtering")
     public SomeBean retrieveSomeBean(){
         return new SomeBean("value1", "value2", "value3");
+    }
+    @GetMapping("/filtering-list")
+    public List<SomeBean> retrieveListSomeBean(){
+        return Arrays.asList(new SomeBean("value1", "value2", "value3"),new SomeBean("value12", "value22", "value32"));
     }
 }
