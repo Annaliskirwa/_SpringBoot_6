4ef15fc8b0ad855e3a6ec481cbab6e88fa880579
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-12 10:31:29 +0300

    added annotations for get/set/constructor

--- src/main/java/com/example/demo/Entities/SomeBean.java
+++ src/main/java/com/example/demo/Entities/SomeBean.java
@@ -1,5 +1,14 @@
 package com.example.demo.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SomeBean {
     private String field1;
     private String field2;
