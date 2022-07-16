7dc65ee01ccdd1955b5b679030b5b1a5a9104f01
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-16 23:07:40 +0300

    picked limits values from configurations

--- src/main/java/com/example/demo/Controller/LimitsController.java
+++ src/main/java/com/example/demo/Controller/LimitsController.java
@@ -1,13 +1,18 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Entities.Limits;
+import com.example.demo.Service.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    @Autowired
+    private Configuration configuration;
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-      return new Limits(1,1000);
+//      return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
